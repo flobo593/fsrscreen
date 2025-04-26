f239b262775b9f44ef4a6af380dd1a74ba397765
Florian Boden <3-flobo@users.noreply.git.wanordemedia.de>
2025-04-26 14:38:40 +0200

    Move endpoint config to config file

--- fsrscreen.php
+++ fsrscreen.php
@@ -11,6 +11,7 @@ License: A "Slug" license name e.g. GPL2
 */
 
 declare(strict_types=1);
+var_dump(retrieveDepartureDataFromVVO());
 
 /**
  * Reads the config file at assets/config.json and returns an array
@@ -48,18 +49,30 @@ function readConfig () : array
  */
 function retrieveDepartureDataFromVVO () : array
 {
+	$dataProviderConfig = readConfig()['dataProvider'];
+	
+	switch($dataProviderConfig['providerId'])
+	{
+		case "VVO-Abfahrtsmonitor":
+		{
+			$uri = $dataProviderConfig['uri'];
+			$method = $dataProviderConfig['method'];
+			$flags = "?".http_build_query($dataProviderConfig['flags']);
+		}
+		default: break;
+	}
 	
 	// just a curl query. Explains itself.
 	$curl = curl_init();
 	
 	curl_setopt_array($curl, [
-		CURLOPT_URL => "http://widgets.vvo-online.de/abfahrtsmonitor/Abfahrten.do?hst=NUP&vz=0&ort=Dresden&lim=20",
+		CURLOPT_URL => $uri.$flags,
 		CURLOPT_RETURNTRANSFER => true,
 		CURLOPT_ENCODING => "",
 		CURLOPT_MAXREDIRS => 10,
 		CURLOPT_TIMEOUT => 30,
 		CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-		CURLOPT_CUSTOMREQUEST => "GET",
+		CURLOPT_CUSTOMREQUEST => $method,
 		CURLOPT_POSTFIELDS => "",
 	]);
 	
